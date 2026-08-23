ddef1b7b0acffa3419e396115cf8dfa90c5372b8
Jorge Mendez Ortega <jorge.mendez.ortega@gmail.com>
2026-08-23 13:48:06 -0600

    feat(app): update 1

--- extras/labels/index.js
+++ extras/labels/index.js
@@ -64,6 +64,7 @@ const CreateLabels = (() => {
      * @return Array.
      */
     const __requestApi = async (labelName = "") => {
+        console.log(`Request URL: https://api.github.com/repos/${REPOSITORY}/labels${labelName}`);
         const REQUEST = await fetch(`https://api.github.com/repos/${REPOSITORY}/labels${labelName}`, __OPTIONS);
         let response = [{ name: labelName, erase: 0 }];
         try {
@@ -90,7 +91,6 @@ const CreateLabels = (() => {
      */
     const __borrar = async () => {
         const LABELS = await __listar();
-        console.log(LABELS);
         let count = 0;
         __OPTIONS.method = "DELETE";
         for (const { name } of LABELS) {
