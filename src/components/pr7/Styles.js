b64966f804901c2faf799ab385ca7a4d07579bda
Jorge Mendez Ortega <jorge.mendez.ortega@gmail.com>
2026-08-24 15:34:09 -0600

    feat(app): pr-007 uso de eventos

---
+++ src/components/pr7/Styles.js
@@ -0,0 +1,11 @@
+const Styles = `
+    <style>
+        :host {}
+        :host form {}
+        :host form input {}
+        :host form input:focus {}
+        :host form button {}
+    </style>
+`;
+
+export default Styles;
